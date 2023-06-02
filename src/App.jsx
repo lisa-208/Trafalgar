c8ca3cc6f3106138b2510fd38c2668797f19de2d
Lisa <jannatul.ferdaus.khan.lisa@g.bracu.ac.bd>
2023-06-02 16:18:39 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <div>
       <div className="container">
-        {/* <Navbar /> */}
+        <Navbar />
         <section id="home">
           <Home/>
         </section>
