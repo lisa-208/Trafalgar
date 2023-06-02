65ab7276326399f0fa915e8d344774f547042c7e
Lisa <jannatul.ferdaus.khan.lisa@g.bracu.ac.bd>
2023-06-02 14:23:01 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <Navbar />
+      <section id="home">
+        <h1>Home</h1>
+        {/* Home content */}
+      </section>
+      <section id="about">
+        <h1>About</h1>
+        {/* About content */}
+      </section>
+      <section id="contact">
+        <h1>Contact</h1>
+        {/* Contact content */}
+      </section>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+const Navbar = () => {
+ 
+    return (
+      <nav>
+        <ul>
+            <li>
+            <a href="#home">Home</a>
+            </li>
+            <li>
+            <a href="#findDoctor">Find a doctor</a>
+            </li>
+            <li>
+            <a href="#apps">Apps</a>
+            </li>
+            <li>
+            <a href="#testimonials">Testimonials</a>
+            </li>
+            <li>
+            <a href="#about">About us</a>
+            </li>
+        </ul>
+      </nav>
+    );
+  };
+
+export default Navbar;
